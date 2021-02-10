7d36001dc075d261cba45619047a04b5d4654cc5
davidko604 <davidko604@gmail.com>
2021-02-09 16:01:52 -0800

    Add star rating and review list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css'
-import { ProductDetails } from './components/ProductDetails'
-import { ReviewDetails } from './components/ReviewDetails'
+import ProductShowPage from './components/ProductShowPage'
 
 function App() {
   return (
     <div className='App'>
-      <ProductDetails
-        title='Good Quality Wrench'
-        description='So good please buy'
-        fullName='John Schmoe'
-        price={7}
-        createdAt={new Date().toString()}
-      />
-      <ReviewDetails
-        rating={4}
-        body='It is as good as they say'
-        reviewerName='Sally Bati'
-        createdAt={new Date().toString()}
-      />
+      <ProductShowPage />
     </div>
   )
 }
